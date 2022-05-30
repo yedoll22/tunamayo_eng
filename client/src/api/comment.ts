527c267a77a72611b37c94f3d2bf11bb0e9bf3d8
Taero-Kim <taero30@gmail.com>
2022-05-30 23:33:04 +0900

    Migration server api state to react-query.
    Migration server api state to react-query.

--- client/src/api/comment.ts
+++ client/src/api/comment.ts
@@ -6,6 +6,18 @@ interface DeleteCommentVariable {
   commentId: number;
 }
 
+interface PatchCommentVariable {
+  toiletId: number;
+  commentId: number;
+  content: string;
+  rating: number;
+}
+
+interface PostCommentVariable {
+  toiletId: number;
+  content: string;
+  rating: number;
+}
 const getAllComments = (toiletId: number | undefined) => {
   return customAxios.get(`/toilets/${toiletId}/comments`);
 };
@@ -24,3 +36,40 @@ const deleteComment = (commentInfo: DeleteCommentVariable) => {
 };
 
 export const useDeleteComment = () => useMutation(deleteComment);
+
+const getMyComment = (toiletId: number, commentId: number) => {
+  return customAxios.get(
+    `/toilets/${toiletId}/comments?commentId=${commentId}`
+  );
+};
+
+export const useCommentQuery = (
+  toiletId: number,
+  commentId: number,
+  successCallback: (data: any) => void
+) =>
+  useQuery(["myComment", commentId], () => getMyComment(toiletId, commentId), {
+    select: (res) => res.data.comment,
+    onSuccess: (data) => successCallback(data),
+  });
+
+const patchComment = (commentInfo: PatchCommentVariable) => {
+  return customAxios.patch(
+    `/toilets/${commentInfo.toiletId}/comments/${commentInfo.commentId}`,
+    {
+      content: commentInfo.content,
+      rating: commentInfo.rating,
+    }
+  );
+};
+
+export const usePatchComment = () => useMutation(patchComment);
+
+const postComment = (commentInfo: PostCommentVariable) => {
+  return customAxios.post(`/toilets/${commentInfo.toiletId}/comments`, {
+    content: commentInfo.content,
+    rating: commentInfo.rating,
+  });
+};
+
+export const usePostComment = () => useMutation(postComment);
