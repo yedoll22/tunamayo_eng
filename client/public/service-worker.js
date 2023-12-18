b9469dd33b1b1cbec49d93d8e1395ee2016855bb
Taero-Kim <taero30@gmail.com>
2023-12-19 03:41:01 +0900

    [알림 콘솔 추가]

--- client/public/service-worker.js
+++ client/public/service-worker.js
@@ -1,6 +1,8 @@
 console.log("Service-worker run...");
 
 self.addEventListener("push", function (event) {
+  console.log("event: ", event);
+  console.log("event.data: ", event.data);
   const payload = event.data ? event.data.text() : "No payload";
   event.waitUntil(
     self.registration.showNotification("Web Push Notification", {
