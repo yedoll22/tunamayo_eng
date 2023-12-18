6cd2c0f47ad4a1d66b0e935176161b022a3bfb12
Taero-Kim <taero30@gmail.com>
2023-12-19 04:09:46 +0900

    서비스 워커 테스트

--- client/public/pwabuilder-sw.js
+++ client/public/pwabuilder-sw.js
@@ -47,3 +47,14 @@ self.addEventListener("fetch", (event) => {
     );
   }
 });
+
+self.addEventListener("push", function (event) {
+  console.log("event: ", event);
+  console.log("event.data: ", event.data);
+  const payload = event.data ? event.data.text() : "No payload";
+  event.waitUntil(
+    self.registration.showNotification("Web Push Notification", {
+      body: payload,
+    })
+  );
+});
